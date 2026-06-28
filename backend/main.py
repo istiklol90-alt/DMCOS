9769448d7a55fa801f77bef76be3e24837f5c9f6
Daler Rajabov <istiklol90@gmail.com>
2026-06-28 22:07:23 +0500

    Stage 3 - First API is running

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "DMC OS is running!"}
+    from fastapi import FastAPI
+
+app = FastAPI(title="DMC OS API")
+
+
+@app.get("/")
+def root():
+    return {
+        "status": "running",
+        "project": "DMC OS"
+    }
