1b7aaad276c2346c25c8a53f350ec317136912a1
Daler Rajabov <istiklol90@gmail.com>
2026-06-29 01:39:20 +0500

    Stage 6 - Users API connected to database

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,15 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database.db import SessionLocal
+from models.user import User
+from schemas.user import UserCreate, UserResponse
 
 router = APIRouter()
 
 
-@router.get("/users")
-def get_users():
-    return []
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@router.post("/users")
-def create_user():
-    return {
-        "message": "User created"
-    }
+@router.get("/users", response_model=list[UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
+
+
+@router.post("/users", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        name=user.name,
+        email=user.email
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
