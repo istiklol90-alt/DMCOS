a85051f6d596bf27ad889069e3dc0c681a82454b
Daler Rajabov <istiklol90@gmail.com>
2026-06-28 23:16:38 +0500

    Stage 4 - Database connected

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,3 @@ from api.routes import router
 app = FastAPI(title="DMC OS API")
 
 app.include_router(router)
-
-
-@app.get("/")
-def root():
-    return {
-        "status": "running",
-        "project": "DMC OS"
-    }
